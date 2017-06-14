421b69fb23054a303f6daafb6f28c2f703640d6f
5nai3r <dzmakr@gmail.com>
2017-06-14 15:22:00 +0100

    Fix Buges
    Fixed Leading Zeros
    Fixed Decimal !
    Now No more possibility to use more than one operator

--- js/app.js
+++ js/app.js
@@ -7,24 +7,41 @@ var converstion = {
         ".":"."
       }
 
+
+var leadingzero = true
+var decimal = false
+var operator = true
+var newexpression = false
+
 $(".btn").click(function(e){
   var value = this.innerHTML;
   if (value == "=") {
     $(".input")[0].innerHTML = math.eval(expression).toString();
     expression = math.eval(expression).toString();
+    newexpression = true;
     return;
   }
-  console.log(this.innerHTML);
-  $(".input")[0].innerHTML += this.innerHTML;
+  if((value == 0 && leadingzero)||(value =="." && decimal)){return}
+  leadingzero = false;
   if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
+    if(newexpression){return;}
     expression += value
+    operator = false;
   }else {
+    if(value == "."){decimal = true;leadingzero = false;operator=false}else {decimal = false}
+    if(operator){return;}
+    leadingzero = true
+    newexpression = false
     expression += converstion[value]
+    operator = true;
   }
-
+  $(".input")[0].innerHTML += this.innerHTML;
 });
 
 $(".clear").click(function(e){
+  leadingzero = true;
+  decimal = false;
+  operator = false;
   var elm = this;
   $(elm).addClass("animated")
   setTimeout(
